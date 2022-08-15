849475d4be6df9f114b0a231324192d0bf663bfb
Mikhail Ishpakhtin <miishp@mail.ru>
2022-08-15 12:11:13 +0300

    added plugins section in build.gradle

---
+++ src/test/java/com/demoqa/ExampleJUnit5Test.java
@@ -0,0 +1,37 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.module.Configuration;
+
+public class ExampleJUnit5Test {
+
+    @BeforeAll
+    static void configure() {
+        System.out.println("this is the @BeforeAll");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("this is the @AfterAll");
+    }
+
+    @BeforeEach
+    void beforeTest(){
+        System.out.println("    this is the @BeforeEach");
+    }
+
+    @AfterEach
+    void afterTest(){
+        System.out.println("    this is the @AfterEach");
+    }
+
+    @Test
+    void firstTest0(){
+        System.out.println("        this is the @Test 0");
+    }
+    @Test
+    void firstTest1(){
+        System.out.println("        this is the @Test 1");
+    }
+}
